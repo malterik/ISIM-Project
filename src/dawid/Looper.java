d5ff7d4a20042d77e93c0f2d4dd5f6fb8e20d618
testo-san <testo-san@134.28.185.57>
2014-12-12 15:48:40 +0100

    Content #1

--- src/dawid/Looper.java
+++ src/dawid/Looper.java
@@ -5,10 +5,30 @@
  */
 package dawid;
 
+import utils.Config;
+import utils.Seed;
+import utils.Voxel;
+
 /**
  *
  * @author testo-san
  */
 public class Looper {
+    
+    public static Voxel [][][] body;
+    public static Seed[] seeds = new Seed[Config.numberOfSeeds];
+    
+    /**
+	 * The Solver Class implements different algorithms to optimize the times a radiation seed stay in the body
+	 * @param body
+	 * The body of the patient
+	 * @param seeds
+	 * The seeds which should be optimized
+	 */
+	
+    public Looper(Voxel [][][] body, Seed[] seeds) {
+		this.body = body;
+		this.seeds = seeds;
+	}
     
 }
