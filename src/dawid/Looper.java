0f5fb9590e25a7739f2531501288c61db61552a2
testo-san <testo-san@134.28.183.9>
2015-01-16 20:54:13 +0100

    SA liefert korrektes globales Ergebnis. Neue Konfigparameter am Start zum Einstellen der Komplexitaet. ToDo: DB und Multithreadkostenfunktion. ToDo ToDo: integrieren in erik.TreatmentPlanner

--- src/dawid/Looper.java
+++ src/dawid/Looper.java
@@ -20,9 +20,14 @@ import java.lang.Math;
 public class Looper {
     
     /**
-     *
-     * @param args
-     */
+ * Creates 
+ *  <strong> Using the default constructor will not work.</strong>
+ * Use <i>new Looper(body,seeds);	</i>
+ * @param  seeds Seed[][][] Necessary for constructor
+ * @param  body Voxel[][][]Necessary for constructor
+ * @return      the image at the specified URL
+ * @see         Image
+ */
     
     public static Voxel [][][] body; //Thobi hat das als ganz einfache Variable in seiner Loesermethode...nicht so OOP
     public static Seed[] seeds = new Seed[Config.SAnumberOfSeeds];
@@ -30,7 +35,9 @@ public class Looper {
 //    public static Seed[] New_state = new Seed[Config.SAnumberOfSeeds];
     public double[] Cur_state = new double[Config.SAnumberOfSeeds];
     public double[] New_state = new double[Config.SAnumberOfSeeds]; // Do I even need this ?
-    private double Cur_cost, New_cost, temperature;
+    public double[] Global_Lowest_state = new double[Config.SAnumberOfSeeds]; // Do I even need this ?
+    
+    private double Cur_cost, New_cost, Global_lowest_cost, temperature;
     
     /**
 	 * WHAT THIS DOES
@@ -49,19 +56,18 @@ public class Looper {
                 this.temperature = temperature;
                 this.Cur_state = Cur_state;
                 this.New_state = New_state;
-                initState();
+                this.Global_lowest_cost = Double.MAX_VALUE;
+                this.Global_Lowest_state = Global_Lowest_state;
 //                LogTool.print("Cur_state : " + Cur_state + " Zeit 2 : " + Zeit2 + " body : " + New_state, "notification");
 //                this.dwelltimes = Looper.seeds().;
                         }
- 
-//    Idea to implement a copy constructor....but this would have to be in the seed class...
-//  Looper copyFoo (Foo foo){
-//  Foo f = new Foo();
-//  //for all properties in FOo
-//  f.set(foo.get());
-//  return f;
-//}
 
+    /**
+ * Copies the Durations from the seeds object that Looper has been constructed with
+ * (i.e. call by value)
+ * @param  Cur_state  a double[] containing the dwelltimes for each seed
+ * @return      Writes to Cur_state, doesnt return anything
+ */
     private void initState() {
          for(int ii=0; ii < Config.SAnumberOfSeeds; ii++)
             {
@@ -69,19 +75,23 @@ public class Looper {
 //                Zeit2 = this.seeds[1].getDurationMilliSec();
 //                Zeit3 = this.seeds[2].getDurationMilliSec();
 //                LogTool.print("Zeit 1 : " + Zeit1 + "Zeit 2 : " + Zeit2 + "Zeit 3 : " + Zeit3, "notification");
-//                LogTool.print("Dwelltime Seed  " + ii + " : " + Cur_state[ii], "notification");
+//                LogTool.print("initState: Dwelltime Seed  " + ii + " : " + Cur_state[ii], "notification");
 //                Cur_state[0] = Zeit1;
 //                Cur_state[1] = Zeit2;
 //                Cur_state[2] = Zeit3;
             }
     }
-    
+    /**
+    * Works. Randomly determines a new state vector
+    * @param New_state Contains randomized dwelltimes
+    * @param Config.SAnumberOfSeeds Contains randomized dwelltimes
+    */
     private void newState() {
          for(int iii=0; iii < Config.SAnumberOfSeeds; iii++)
             {
                 New_state[iii] = RandGenerator.randDouble(0, 10);
-//                LogTool.print("Dwelltime Seed  " + iii + " : " + Cur_state[iii], "notification");
-//                LogTool.print("Zeit " + iii + " : " + New_state[iii], "notification");
+//                LogTool.print("newState: Dwelltime Seed  " + iii + " : " + Cur_state[iii], "notification");
+//                LogTool.print("newState: Zeit " + iii + " : " + New_state[iii], "notification");
             }
     }
     
@@ -97,30 +107,65 @@ public class Looper {
         return Cur_cost;
     }
 
+    public void setGlobal_Lowest_state(double[] Global_Lowest_state) {
+        this.Global_Lowest_state = Global_Lowest_state;
+    }
+
+    public void setGlobal_lowest_cost(double Global_lowest_cost) {
+        this.Global_lowest_cost = Global_lowest_cost;
+    }
+
+    public double getGlobal_lowest_cost() {
+        return Global_lowest_cost;
+    }
+
+    public double[] getGlobal_Lowest_state() {
+        return Global_Lowest_state;
+    }
+    
     public void setCur_cost(double Cur_cost) {
         this.Cur_cost = Cur_cost;
     }
-    
+/**
+ * 
+ * @return String of the new state vector entries 
+ */
     public String getNew_state() {
         String NewState = new String();
-        NewState = "1: " + New_state[0] + " 2: " + New_state[1] + " 3: " + New_state[2] + "";
+        for (int aa = 0; aa < Config.SAnumberOfSeeds; aa++) {
+            NewState = NewState.concat(" " + aa + ") " + New_state[aa]);
+            }
+//        NewState = "1: " + New_state[0] + " 2: " + New_state[1] + " 3: " + New_state[2] + "";
         return NewState;
     }
-
+/**
+ * 
+ * @param New_state 
+ */
     public void setNew_state(double[] New_state) {
         this.New_state = New_state;
     }
-
+/**
+ * 
+ * @return String containing a <i>formatted</i> output of the dwelltimes
+ */
     public String getCur_state() {
         String CurState = new String();
-        CurState = "1: " + Cur_state[0] + " 2: " + Cur_state[1] + " 3: " + Cur_state[2] + "";
+//        CurState = "1: " + Cur_state[0] + " 2: " + Cur_state[1] + " 3: " + Cur_state[2] + "";
+        for (int aa = 0; aa < Config.SAnumberOfSeeds; aa++) {
+            CurState = CurState.concat(" " + aa + ") " + Cur_state[aa]);
+            }
         return CurState;
     }
-    
+// following works    
     public void setCur_state(double[] Cur_state) {
         this.Cur_state = Cur_state;
+        
     }
-    
+/**
+ * 
+ * @return The Seed[] of seeds
+ */
     public static Seed[] getSeed()
 	{
 		return seeds;
@@ -131,49 +176,68 @@ public class Looper {
          * It contains the metropolis loop
 	 */
     public void solveSA() {
-        setCur_cost(cost());
-        LogTool.print("SolveSA: Cur Cost : " + Cur_cost,"notification");
-//        this.initState();
-//        LogTool.print("SolveSA: Initial State : A)" + Looper.seeds[0].getDurationMilliSec() + " B) " + seeds[1].getDurationMilliSec() + " C) " + seeds[2].getDurationMilliSec(),"notification");
-        LogTool.print("SolveSA: Initial State : A)" + Cur_state[0] + " B) " + Cur_state[1] + " C) " + Cur_state[2],"notification");
-     //[Newstate] with random dwelltimes
-        newState(); 
-        LogTool.print("SolveSA: New State : A)" + New_state[0] + " B) " + New_state[1] + " C) " + New_state[2],"notification");
-        setNew_cost(cost());
-        LogTool.print("SolveSA: New Cost : " + New_cost,"notification");
-        /**
-            * MetropolisLoop - 
-	 */
-        
-	for(int x=0;x<Config.NumberOfMetropolisRounds;x++) {   
-//            LogTool.print("SolveSA Iteration " + x + " Curcost " + Cur_cost + " Newcost " + New_cost,"notification");
-           if ((Cur_cost - New_cost)>0) { // ? die Kosten
-//                    Cur_state = New_state;
-//                    LogTool.print("Cost CurisNull ? ->  " + Cur_state + "","notification");
-                      LogTool.print("Fall 1","notification");
-                      LogTool.print("SolveSA: NewCost : " + this.getNew_cost(),"notification");
-                      LogTool.print("SolveSA: CurCost : " + this.getCur_cost(),"notification");
-                      LogTool.print("SolveSA Cost delta " + (Cur_cost - New_cost) + "","notification");
-                      Cur_state = New_state;
-                } else if (Math.exp(-(Cur_cost - New_cost)/temperature)> RandGenerator.randDouble(0.01, 0.99)) {
-                    Cur_state = New_state; 
-                    LogTool.print("Fall 2: Zufallsgenerierter Zustand traegt hoehere Kosten als vorhergehender Zustand. Iteration: " + x,"notification");
-                }
-           temperature = temperature-1;
-           if (temperature==0)  {
-               break;
-           }
-           newState();
-           setNew_cost(cost());
-        }
-        LogTool.print("SolveSA: Solution : A)" + this.getCur_state(),"notification");
-        LogTool.print("SolveSA: NewCost : " + this.getNew_cost(),"notification");
-        LogTool.print("SolveSA: CurCost : " + this.getNew_cost(),"notification");
-        //
-//        return New_Cost;
-        
-    }
+        for (int ab = 0; ab < Config.NumberOfMetropolisResets; ab++) {            
+            LogTool.print("SolveSA: Initial State : A)" + Cur_state[0] + " B) " + Cur_state[1] + " C) " + Cur_state[2],"notification");
+            this.initState();
+            LogTool.print("SolveSA: Initial State : A)" + Cur_state[0] + " B) " + Cur_state[1] + " C) " + Cur_state[2],"notification");
+            setCur_cost(cost());
 
+            /* [Newstate] with random dwelltimes */
+            newState(); 
+            LogTool.print("SolveSA: New State : A)" + New_state[0] + " B) " + New_state[1] + " C) " + New_state[2],"notification");
+            setNew_cost(cost());
+            LogTool.print("SolveSA: New Cost : " + New_cost,"notification");
+            double random_double = RandGenerator.randDouble(0.01, 0.99);
+
+            /**
+                * MetropolisLoop - 
+             */
+
+            for(int x=0;x<Config.NumberOfMetropolisRounds;x++) {   
+    //            break;
+    //            LogTool.print("SolveSA Iteration " + x + " Curcost " + Cur_cost + " Newcost " + New_cost,"notification");
+               if ((Cur_cost - New_cost)>0) { // ? die Kosten
+                   LogTool.print("Fall 1","notification");
+                   if (Config.SAdebug) {                      
+                          LogTool.print("SolveSA: NewCost : " + this.getNew_cost(),"notification");
+                          LogTool.print("SolveSA: CurCost : " + this.getCur_cost(),"notification");
+                          LogTool.print("SolveSA Cost delta " + (Cur_cost - New_cost) + "","notification");
+                   }
+                          Cur_state = New_state;
+                          Cur_cost = New_cost;
+                    } else if (Math.exp(-(Cur_cost - New_cost)/temperature)> random_double) {
+                        Cur_state = New_state;
+                        Cur_cost = New_cost;
+                        if (Config.SAdebug) {
+                        LogTool.print("SolveSA: NewCost : " + this.getNew_cost(),"notification");
+                        LogTool.print("SolveSA: CurCost : " + this.getCur_cost(),"notification");
+                        }
+                        LogTool.print("Fall 2: Zufallsgenerierter Zustand traegt hoehere Kosten als vorhergehender Zustand. Iteration: " + x,"notification");
+
+                    }
+               temperature = temperature-1;
+               if (temperature==0)  {
+                   break;
+               }
+               random_double = RandGenerator.randDouble(0.01, 0.99);
+               newState();
+               setNew_cost(cost());
+            }
+            if (Cur_cost<Global_lowest_cost) {
+                this.setGlobal_lowest_cost(Cur_cost);
+                this.setGlobal_Lowest_state(this.Cur_state);
+            }
+            LogTool.print("SolveSA: Last Candidate Solution : " + this.getNew_state(),"notification");
+            LogTool.print("SolveSA: Best Solution : " + this.getCur_state(),"notification");
+            LogTool.print("SolveSA: NewCost : " + this.getNew_cost(),"notification");
+            LogTool.print("SolveSA: CurCost : " + this.getCur_cost(),"notification");        
+        }
+    }
+/**
+ * Sets the irradiation of the Tumor volume by iterating over its volume.
+ * 
+ * @return Returns the cost as a SSD (Sum of squared differences)
+ */
     public double cost() {
         double diff=0;
         double intensity=0;
@@ -198,18 +262,21 @@ public class Looper {
 		}
         return Math.sqrt(diff);
 //        return Math.random();
-    }
-
-//    private void setFitnessValue(double cost) {
-//        try {
-//            
-//            return cost;
-//            
-//        } catch (Exception e) {
-//            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//        }
-//        
-//    }
+    }    
     
+    public void rausfindenWarumCUrCostsichnichtaendert() {
+        LogTool.print("SolveSA: Initial State : A)" + Cur_state[0] + " B) " + Cur_state[1] + " C) " + Cur_state[2],"notification");
+        LogTool.print("SolveSA: Cur Cost : " + Cur_cost + " should be " + cost(),"notification");
+        initState();
+        LogTool.print("SolveSA: Next Initial State : A)" + Cur_state[0] + " B) " + Cur_state[1] + " C) " + Cur_state[2],"notification");
+        LogTool.print("SolveSA: Cur Cost : " + Cur_cost + " should be " + cost(),"notification");
+        
+        LogTool.print("SolveSA: Initial State : A)" + Cur_state[0] + " B) " + Cur_state[1] + " C) " + Cur_state[2],"notification");
+        newState(); 
+        LogTool.print("SolveSA: New State : A)" + New_state[0] + " B) " + New_state[1] + " C) " + New_state[2],"notification");
+        setNew_cost(cost());
+        LogTool.print("SolveSA: New Cost : " + New_cost,"notification");
+        /* [Newstate] with random dwelltimes */        
+    }
     
 }
